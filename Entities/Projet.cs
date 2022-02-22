bcf7155924167d23dc8354d8fc200cc7d216072c
romaric <romaasmiakas@hotmail.fr>
2022-02-22 13:54:39 +0100

    Add App section

---
+++ Entities/Projet.cs
@@ -0,0 +1,10 @@
+
+    public class Project
+    {
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+
+        public string? Uri { get; set; }
+    }
