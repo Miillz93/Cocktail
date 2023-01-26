5f1cb24b597f9d9c0ab89d9fbe35feed0f7d9cae
miillz <romaric.miaka.sio@gmail.com>
2023-01-26 19:11:35 +0100

    saving

--- Entities/Cocktail/DrinkRoot.cs
+++ Entities/Cocktail/DrinkRoot.cs
@@ -3,5 +3,7 @@ namespace BlazorPlaygroundWasm.Entities.Cocktail;
 
    public class DrinkRoot
     {
+        public static string _urlBase = "https://www.thecocktaildb.com/api/json/v1/1/";
+
         public List<Drink> drinks { get; set; }
     }
--- Entities/Cocktail/UriCocktail.cs
+++
@@ -1,8 +0,0 @@
-namespace BlazorPlaygroundWasm.Entities.Cocktail;
-
-
-public class UriCocktail
-{
-    public static string urlBase = "https://www.thecocktaildb.com/api/json/v1/1/";
-  
-}
