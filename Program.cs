f45a2f6372935072e0e381aeb7d8b14a3929b9c2
miillz <romaric.miaka.sio@gmail.com>
2023-03-24 11:34:56 +0100

    add cocktail service

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorPlaygroundWasm;
 using MudBlazor.Services;
+using BlazorPlaygroundWasm.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ICocktailService, CocktailService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
