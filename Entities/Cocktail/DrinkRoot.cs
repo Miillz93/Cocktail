990a73ba4913ac698a28d641755a6b01e363ccbc
miillz <romaric.miaka.sio@gmail.com>
2023-05-17 19:27:15 +0200

    fix

--- Entities/Cocktail/DrinkRoot.cs
+++ Entities/Cocktail/DrinkRoot.cs
@@ -3,8 +3,6 @@ namespace BlazorPlaygroundWasm.Entities.Cocktail;
 
    public class DrinkRoot
     {
-        public static string _urlBase = "https://www.thecocktaildb.com/api/json/v1/1/";
-
         public List<Drink> drinks { get; set; }
         public List<Ingredient> ingredients {get;set;}
     }
