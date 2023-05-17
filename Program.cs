54cb5e033b423e80bfca68cd92ac43dec00877e7
miillz <romaric.miaka.sio@gmail.com>
2023-05-17 19:28:04 +0200

    uri

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/") });
+builder.Services.AddHttpClient("cocktail", httpClient => {
+    httpClient.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/");
+});
 builder.Services.AddSingleton<ICocktailService, CocktailService>();
 builder.Services.AddMudServices();
 
